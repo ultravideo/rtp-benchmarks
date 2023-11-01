baea58f4061ef2a923605598dc2cf84eda4a5db8
Heikki Tampio <heikki.tampio@tuni.fi>
2023-11-01 16:00:12 +0200

    atlas: Send VPCC parameter set NAL units in throughput tests too

--- uvgrtp/vpcc_sender.cc
+++ uvgrtp/vpcc_sender.cc
@@ -126,6 +126,7 @@ void sender_func(uvgrtp::media_stream* stream, const char* cbuf, int fmt, float 
     uint64_t period = (uint64_t)((1000 / (double)fps) * 1000);
     uint8_t* bytes = (uint8_t*)cbuf;
     rtp_error_t ret = RTP_OK;
+    bool param_set = false; // For parameter set NAL units, special treatment
 
     size_t bytes_sent = 0;
     // start the sending test
@@ -134,17 +135,21 @@ void sender_func(uvgrtp::media_stream* stream, const char* cbuf, int fmt, float 
 
     for (auto& p : units) {
         for (auto i : p.nal_infos) {
+            param_set = false; // Check the type of this NAL unit
             uint8_t nalu_t = (bytes[i.location] >> 1) & 0x3f;
-            if(fmt == V3C_AD && nalu_t > 35 ) { // Atlas streams: Skip non-ACL NAL units
-                continue;
+            if(fmt == V3C_AD && nalu_t > 35 ) {
+                param_set = true;
             }
-            else if (nalu_t >= 32 && nalu_t <= 34) { // HEVC streams: Skip parameter set NAL units
-                continue;
+            else if (nalu_t >= 32 && nalu_t <= 34) {
+                param_set = true;
             }
             if ((ret = stream->push_frame(bytes + i.location, i.size, RTP_NO_H26X_SCL)) != RTP_OK) {
                 std::cout << "Failed to send RTP frame!" << std::endl;
             }
             bytes_sent += i.size;
+            if(param_set) { // If this is a parameter set NALU, immediately send the next NAL unit
+                continue;
+            }
             temp_nalu++;
             if (fmt == V3C_GVD || fmt == V3C_AVD) { // If this is GVD or AVD stream, send 4 frames as fast as we can, then wait for frame interval
                 if(temp_nalu < 4) {
